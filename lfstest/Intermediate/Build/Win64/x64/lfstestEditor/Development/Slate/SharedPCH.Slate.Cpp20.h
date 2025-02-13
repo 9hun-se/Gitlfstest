9f577ea7a59bb5cfb6f69e6b68ea04605405b8a4
9hun-se <kevin990114@gmail.com>
2025-02-13 19:25:01 +0900

    .

---
+++ lfstest/Intermediate/Build/Win64/x64/lfstestEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "C:/UNREAL/lfstest/Intermediate/Build/Win64/x64/lfstestEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
---
+++ lfstest/Intermediate/Build/Win64/x64/lfstestEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "C:/UNREAL/lfstest/Intermediate/Build/Win64/x64/lfstestEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
